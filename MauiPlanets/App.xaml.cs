f01f1a6b142e31955638a2f64868967a5918e30a
Eduardo Nogueira <eduardodudunog@gmail.com>
2023-03-19 23:03:28 -0300

    feat: add start page

---
+++ MauiPlanets/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using MauiPlanets.Views;
+
+namespace MauiPlanets;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new NavigationPage(new StartPage());
+	}
+}
---
+++ MauiPlanets/MauiProgram.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Controls.PlatformConfiguration;
+using Microsoft.Maui.LifecycleEvents;
+
+namespace MauiPlanets;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+                fonts.AddFont("Montserrat-Medium.ttf", "RegularFont");
+                fonts.AddFont("Montserrat-SemiBold.ttf", "MediumFont");
+                fonts.AddFont("Montserrat-Bold.ttf", "BoldFont");
+            })
+			.ConfigureLifecycleEvents(events =>
+			{
+#if ANDROID
+                events.AddAndroid(android => android
+                    .OnCreate((activity, bundle) => MakeStatusBarTranslucent(activity)));
+
+                static void MakeStatusBarTranslucent(Android.App.Activity activity)
+                {
+                    activity.Window.SetFlags(Android.Views.WindowManagerFlags.LayoutNoLimits, Android.Views.WindowManagerFlags.LayoutNoLimits);
+
+                    activity.Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+
+                    activity.Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+                }
+#endif
+            });
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
